80d3df43c5ed232ffa9cc7faff43e5058795ce15
katrina <katrina.m.ellison@gmail.com>
2014-05-22 19:01:46 -0500

    Added flexible port for Heroku

--- better_off_live.py
+++ better_off_live.py
@@ -1,3 +1,6 @@
+from better_off_live.main import app
+
 if __name__ == "__main__":
-    from better_off_live.main import app
-    app.run()
+    # Bind to PORT if defined (by Heroku), otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
